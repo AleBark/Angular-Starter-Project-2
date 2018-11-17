cc082a8008ccc2b78c7a0f28c49c3fe30a907661
Ale Bark <ale.bark_bruneri@hotmail.com>
2018-11-17 19:35:01 -0200

    Providers and service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { OffersSerice } from '../offers.service';
 
 @Component({
   selector: 'ang-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OffersSerice]
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  private offerService: OffersSerice;
+
+  constructor(offerService: OffersSerice) {
+    this.offerService = offerService;
+  }
 
   ngOnInit() {
+    console.log (this.offerService.getOffers());
   }
 
 }
---
+++ src/app/offers.service.ts
@@ -0,0 +1,6 @@
+export class OffersSerice {
+
+    public getOffers(): Array<String> {
+        return ['Dishcloth-01', 'Dishcloth-02', 'Dishcloth-03'];
+    }
+}
